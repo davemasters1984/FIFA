5799d5af53a070938c85d46adb329160abfc7ad8
Dave Masters <davemasters@redweb.com>
2016-12-12 14:11:30 +0000

    move raven extensions to separate file

--- FIFAData/Helpers/TeamAssigner.cs
+++ FIFAData/Helpers/TeamAssigner.cs
@@ -98,33 +98,4 @@ namespace FIFAData
             _assignments.AddRange(handicappedAssignments);
         }
     }
-
-    public static class RavenExtensions
-    {
-        public static List<T> GetAll<T>(this IDocumentSession session)
-        {
-            const int size = 1024;
-            int page = 0;
-
-            RavenQueryStatistics stats;
-            List<T> objects = session.Query<T>()
-                                  .Statistics(out stats)
-                                  .Skip(page * size)
-                                  .Take(size)
-                                  .ToList();
-
-            page++;
-
-            while ((page * size) <= stats.TotalResults)
-            {
-                objects.AddRange(session.Query<T>()
-                             .Skip(page * size)
-                             .Take(size)
-                             .ToList());
-                page++;
-            }
-
-            return objects;
-        }
-    }
 }
---
+++ FIFAData/RavenExtensions/Paging.cs
@@ -0,0 +1,38 @@
+﻿using Raven.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIFAData
+{
+    public static class PagingExtensions
+    {
+        public static List<T> GetAll<T>(this IDocumentSession session)
+        {
+            const int size = 1024;
+            int page = 0;
+
+            RavenQueryStatistics stats;
+            List<T> objects = session.Query<T>()
+                                  .Statistics(out stats)
+                                  .Skip(page * size)
+                                  .Take(size)
+                                  .ToList();
+
+            page++;
+
+            while ((page * size) <= stats.TotalResults)
+            {
+                objects.AddRange(session.Query<T>()
+                             .Skip(page * size)
+                             .Take(size)
+                             .ToList());
+                page++;
+            }
+
+            return objects;
+        }
+    }
+}
